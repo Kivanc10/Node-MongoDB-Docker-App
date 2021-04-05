ee4a99f87c2b7de566290d26faf388943eae4db9
Kıvanc Aydogmus <kroytun77@gmail.com>
2021-04-06 01:15:47 +0300

    connect url has been changed and yml file has been constructed

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/mini-rest-api",{
+mongoose.connect("mongodb://mongo:27017/mini-rest-api",{
     useNewUrlParser : true,
     useCreateIndex : true,
     useFindAndModify : true,
