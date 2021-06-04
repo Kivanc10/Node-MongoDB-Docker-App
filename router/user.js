14e77f9ec095f649d6a67afe7f83b795ce66a5e3
Kıvanc Aydogmus <kroytun77@gmail.com>
2021-06-04 22:57:34 +0300

    unnecessary pieces of code has been removed

--- router/user.js
+++ router/user.js
@@ -5,18 +5,6 @@ const auth = require("../middleware/auth")
 
 
 
-// router.post("/users",async (req,res) => { // sign up
-//     const user = new User(req.body)
-//     try {
-        
-//         await user.save() // first save
-//         const token = await user.generateAuthToken() // add user's token to new session and save them
-
-//         res.status(201).send({user,token})
-//     } catch (error) {
-//         res.status(400).send(error)
-//     }
-// })
 
 router.post("/users",async (req,res) => { // sign up
    
@@ -102,19 +90,6 @@ router.patch("/users/me",auth,async (req,res) => {
     }
 })
 
-
-router.delete("/users/:id",async (req,res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (error) {
-        res.status(500).send()
-    }
-})
 
 
 module.exports = router;
