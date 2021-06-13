bf0aa0d0bb5cdd3ec3b58353fa2a0d41b74377bb
Kıvanc Aydogmus <kroytun77@gmail.com>
2021-06-13 19:57:05 +0300

    database url is changed

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 //mongo --> 127.0.0.1
-mongoose.connect("mongodb://127.0.0.1:27017/mini-rest-api",{
+mongoose.connect("mongodb://mongo/mini-rest-api",{
     useNewUrlParser : true,
     useCreateIndex : true,
     useFindAndModify : true,
