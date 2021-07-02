63e0cae9f4dedf655830e936193190005c1753f7
Kıvanc Aydogmus <kroytun77@gmail.com>
2021-07-02 14:51:27 +0300

    test request handler is append

--- router/task.js
+++ router/task.js
@@ -113,40 +113,40 @@ router.delete("/tasks/:id", auth, async (req, res) => {
 // router.delete("/tasks/me", auth, async (req, res) => {
 //     try {
 //         const tasks = await Task.deleteMany({owner : req.user._id})
-//         if(!tasks) {
-//             return res.status(404).send("There is no task to remove")
-//         }
+//         // if(!tasks) {
+//         //     return res.status(404).send("There is no task to remove")
+//         // }
 //         res.status(200).send("Tasks belong the user were deleted succesfully")
 //     } catch (error) {
 //         res.status(204).send(error)
 //     }
 // })
 
-router.delete("/tasks/me", auth, async (req, res) => {
-    try {
-        const tasks = await Task.find({owner : req.user._id})
-        if(!tasks) {
-            return res.status(404).send("There is no task to show")
-        }
-        tasks = []
-        await tasks.save()
-        res.status(200).send("Tasks belong the user were deleted succesfully")
-    } catch (error) {
-        res.status(500).send()
-    }
-})
+// router.delete("/tasks/me", auth, async (req, res) => {
+//     try {
+//         const tasks = await Task.find({owner : req.user._id})
+//         if(!tasks) {
+//             return res.status(404).send("There is no task to show")
+//         }
+//         tasks = []
+//         await tasks.save()
+//         res.status(200).send("Tasks belong the user were deleted succesfully")
+//     } catch (error) {
+//         res.status(500).send()
+//     }
+// })
 
-router.get("/tasks/another",auth,async (req,res) => {
-    try {
-        const tasks = await Task.find({owner : req.user._id})
-        if(!tasks) {
-            return res.status(404).send("There is no task to show")
-        }
-        res.status(200).send(tasks)
-    } catch (error) {
-        res.status(500).send()
-    }
-})
+// router.get("/tasks/another",auth,async (req,res) => {
+//     try {
+//         const tasks = await Task.find({owner : req.user._id})
+//         if(!tasks) {
+//             return res.status(404).send("There is no task to show")
+//         }
+//         res.status(200).send(tasks)
+//     } catch (error) {
+//         res.status(500).send()
+//     }
+// })
 
 
 module.exports = router;
